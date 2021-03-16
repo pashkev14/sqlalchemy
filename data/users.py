acf0df42479a9f0c0e37232c457aec2b3e27376a
Yandex Liceum 1 <pavsav.04@gmail.com>
2021-03-16 17:14:36 +0500

    commit

---
+++ data/users.py
@@ -0,0 +1,27 @@
+import sqlalchemy
+from sqlalchemy import orm
+from data.db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String)
+    address = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    job = orm.relation("Jobs", back_populates="team_lead")
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
