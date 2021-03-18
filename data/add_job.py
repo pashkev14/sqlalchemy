7c17cf07e7dc7522da1c1fc5654b4cb4123baced
pashkevich <pavsav.04@gmail.com>
2021-03-18 21:26:38 +0500

    added register

---
+++ data/add_job.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, BooleanField
+
+
+class AddJobForm(FlaskForm):
+    job = StringField('Название работы')
+    team_leader = StringField('ID руководителя')
+    work_size = StringField('Объем работы')
+    collaborators = StringField('ID участников')
+    is_finished = BooleanField('Работа закончена')
+    submit = SubmitField('Добавить')
+
--- show form auth.py
+++ show form auth.py
@@ -39,6 +39,34 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User()
+        user.surname = form.surname
+        user.name = form.name
+        user.age = form.age
+        user.position = form.position
+        user.speciality = form.speciality
+        user.address = form.address
+        user.email = form.email
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 @app.route('/log')
 def show_db():
     db_session.global_init("db/jobs.db")
