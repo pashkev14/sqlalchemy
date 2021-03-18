e2662ab3b5acfb6b97b6d3d57ce99b0d2baea218
pashkevich <pavsav.04@gmail.com>
2021-03-18 18:26:15 +0500

    task 3

--- show form auth.py
+++ show form auth.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_login import LoginManager, login_user, logout_user, login_required
 from werkzeug.utils import redirect
 from data import db_session
@@ -32,39 +32,20 @@ def login():
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return redirect("/")
+            return redirect("/log")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User()
-        user.surname = form.surname.data
-        user.name = form.name.data
-        user.age = form.age.data
-        user.position = form.position.data
-        user.speciality = form.speciality.data
-        user.address = form.address.data
-        user.email = form.email.data
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
+@app.route('/log')
+def show_db():
+    db_session.global_init("db/jobs.db")
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs)
+    css = url_for('static', filename='css/base.css')
+    return render_template("log.html", css=css, jobs=jobs)
 
 
 @app.route('/logout')
